8dfe933e269b9acd2781509aa0c232b247a348e2
Vicente Meller <vicente.melleler@uc.cl>
2022-08-18 16:30:42 -0400

    AF1.2

--- AF1/cargar.py
+++ AF1/cargar.py
@@ -2,11 +2,19 @@
 # los datos vienen en este orden el el .csv:
 # nombre,categoria,tiempo_preparacion,precio,ingrediente_1,...,ingrediente_n
 def cargar_platos(ruta_archivo: str) -> list:
-    pass
+    with open(ruta_archivo, "r") as file:
+        lista = file.readlines()
+        file.close()
+        platos = []
+        for linea in lista:
+            platos.append(linea.strip())
+        
 
-
+        print(platos)
 # --- EXPLICACION --- #
 # los datos vienen en este orden el el .csv:
 # nombre,cantidad
 def cargar_ingredientes(ruta_archivo: str) -> dict:
     pass
+
+cargar_platos("AF1/platos.csv")
